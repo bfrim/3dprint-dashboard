309c2ae1e76d1501068c455d66e8b258fe409fe6
Barnabé Frimaudeau <frimaudeau.barnabe@gmail.com>
2025-06-14 20:05:46 -0400

    Add start-print.html

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/start-print")
+def start_print():
+    return render_template("start-print.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
